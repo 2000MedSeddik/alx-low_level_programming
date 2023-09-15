a74c0cb2a4f7df44364d3748c6ff59d505a2d228
2000MedSeddik <seddikmardi@gmail.com>
2023-09-15 02:15:37 -0700

    1-init_dog.c.1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,18 +1,17 @@
-#include <stdlib.h>
+#include <stdio.h>
 #include "dog.h"
 
 /**
-  * init_dog - Initializes a dog structure
-  * @d: A dog structure.
-  * @name: The name of the dog.
-  * @age: The age of the dog.
-  * @owner: The owner of the dog.
-  *
-  * Return: Nothing
-  */
+ * init_dog - initialize a variable of type struct dog
+ * @d: dog's details
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
